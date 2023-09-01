9939a9fbe806f8b4a0e03254d2e57cdc3094ca35
Icer <liangshuangde@163.com>
2023-09-01 21:27:03 +0800

    format code

--- src/data/levels.js
+++ src/data/levels.js
@@ -1,12 +1,11 @@
-import levels from './levels.json'
+import levels from "./levels.json";
 
-export const levelData = levels
-.map(_=>({
-  levelId:_["Level ID"],
-  levelName:_["Level Name"],
-  type:_["Type"],
-  name:_["Name"],
-  details:_["Details"],
-  location:_["Location"],
-  locationHints:_["Location hints"],
+export const levelData = levels.map((_) => ({
+  levelId: _["Level ID"],
+  levelName: _["Level Name"],
+  type: _["Type"],
+  name: _["Name"],
+  details: _["Details"],
+  location: _["Location"],
+  locationHints: _["Location hints"],
 }));
